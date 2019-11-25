bb3dbfc966e1930788bfd161408b71395ccf2431
beenj <beenjoyhwang@naver.com>
2019-11-25 22:23:02 +0900

    test

---
+++ src/main/java/kr/ac/mjc/game/Direction_R_St_End.java
@@ -0,0 +1,16 @@
+package kr.ac.mjc.game;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Direction_R_St_End extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction__r__st__end);
+
+
+    }
+}
